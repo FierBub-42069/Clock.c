3221f57606deadf2c38fdfd7ea827ff9c8766f90
Ameya Deshpande <97083659+FierBub-42069@users.noreply.github.com>
2022-12-04 10:15:59 +0530

    Update clock.c

--- clock.c
+++ clock.c
@@ -1,7 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<unistd.h>
-// #include<windows.h>
 
 int main() {
 	int h=0, m=0, s=0;
@@ -12,7 +11,7 @@ int main() {
 	switch(ch) {
 		case 1:
 			while(1) {
-				system("cls");		// for unistd.h , use "clear" | for windows.h , use "cls"
+				system("clear");
 				printf("\n Stopwatch: \t ");
 				printf("%02d:%02d:%02d",h,m,s);
 				s++;
@@ -24,28 +23,31 @@ int main() {
 					h++;
 					m=0;
 				}
-				sleep(1);		// for windows.h -> Sleep(*time_in_milliseconds*) [ie, Sleep(1000)] | for unistd.h -> sleep(*time_in_seconds*) [ie, sleep(1)]
+				sleep(1);
 			}
 			break;
 		case 2:
 			printf("\n Enter time in 'hh:mm:ss' format:\t");
 			scanf("%d%d%d",&h,&m,&s);
-			while(h>=0 && m>=0 && s>= 0) {
-				system("cls");		// for unistd.h , use "clear" | for windows.h , use "cls"
+			while(h>=0 && m>=0 && s>=0) {
+				system("clear");
 				printf("\n Timer: \t ");
 				printf("%02d:%02d:%02d",h,m,s);
 				s--;
-				if(s<0) {
+				if(s<0 && m>0) {
 					m--;
 					s=59;
 				}
-				if(m<0) {
+				if(m<0 && h>0) {
 					h--;
 					m=59;
 				}
-				sleep(1);		// for windows.h -> Sleep(*time_in_milliseconds*) [ie, Sleep(1000)] | for unistd.h -> sleep(*time_in_seconds*) [ie, sleep(1)]
-				if(s < 0 && m < 0 && h < 0)
-					exit(0);
+				sleep(1);
+			}
+			if(s < 0) {
+				system("clear");
+				printf("\n Timer completed!");
+				exit(0);
 			}
 			break;
 		default:
